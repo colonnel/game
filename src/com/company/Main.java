694856340ac9bd99ef1a36c621053666f6f88a78
colonnel <serg.berezin@gmail.com>
2016-02-28 22:05:00 +0200

    111

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,85 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        char[] abc = createArrAbc();
+        int rl = rightLetter(abc);
+        int indUserLetter = userLetter(abc);
+        equalLetters(abc, indUserLetter,rl);
+
+
+    }
+
+    //create Arr
+    private static char[] createArrAbc() {
         char[] abc = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        //String abc = "abcdefghijklmnopqrstuvwxyz";
+        return abc;
+    }
+
+//    private static int lengthAbc(char[] abc) {
+//        int x = abc.length;
+//        return x;
+//    }
+
+    //input correct letter
+    private static int rightLetter(char[] abc) {
 
         Random r = new Random();
-        int rl = r.nextInt(27);
+        int rl = r.nextInt(abc.length);
+        return rl;
+    }
 
+    //getting letter from user
+    private static int userLetter(char[] abc) {
         System.out.println("Введите букву латинского алфавита:");
+        Scanner sc = new Scanner(System.in);
+        String inputLt = sc.next();
+        char inLetter = inputLt.charAt(0);
+        int indUserLetter = -1;
         for (int i = 0; i < abc.length; i++) {
-
-            Scanner sc = new Scanner(System.in);
-            String inputLt = sc.nextLine();
-            char inLetter = inputLt.charAt(0);
-            for (int j = 0; j < abc.length; j++) {
-                if (inLetter == abc[j]) {
-                    rl = j;
-                }
-            }
-            if (rl == i) {
-                System.out.println("Угадали!!!");
-            } else if (rl<i) {
-                System.out.println("Не угадали. Буква дальше по алфавиту");
-            } else {
-                System.out.println("Не угадали. Буква ближе по алфавиту");
+            if (inLetter == i) {
+                indUserLetter = i;
             }
         }
+        return indUserLetter;
+    }
+
+    //equal letters
+    private static void equalLetters(char[] abc, int indUserLetter, int rl) {
+        if (rl==-1) {
+            System.out.println("Incorrect letter");
+        } else if (indUserLetter == rl) {
+            System.out.println("Вы победили!!!");
+        } else if (indUserLetter < rl) {
+            System.out.println("Буква дальше по алфавиту");
+        } else if (indUserLetter > rl) {
+            System.out.println("Буква ближе по алфавиту");
+        }
+
+//    System.out.println("Введите букву латинского алфавита:");
+//    for(
+//    int i = 0;
+//    i<abc.length;i++)
+//
+//    {
+//
+//        Scanner sc = new Scanner(System.in);
+//        String inputLt = sc.next();
+//        char inLetter = inputLt.charAt(0);
+//        for (int j = 0; j < abc.length; j++) {
+//            if (inLetter == abc[j]) {
+//                rl = j;
+//            }
+//        }
+//        if (rl == i) {
+//            System.out.println("Угадали!!!");
+//        } else if (rl < i) {
+//            System.out.println("Не угадали. Буква дальше по алфавиту");
+//        } else {
+//            System.out.println("Не угадали. Буква ближе по алфавиту");
+//        }
+//    }
 
 
     }
